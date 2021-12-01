ccd9d17230b5c1e2d5c4b08b01ed1a40106486f3
inwang <inwang@umich.edu>
2021-11-30 20:06:06 -0500

    Writing the code to evaluate input

---
+++ diagnosis.py
@@ -0,0 +1,41 @@
+import sys
+
+def input_to_dict(filename):
+    dict = {}
+    with open(filename) as f:
+        line_num = 0
+        disease_name = ""
+        for line in f:
+            if line_num % 2 == 0:
+                disease_name = line
+            else:
+                symptoms = line.split(", ")
+                dict.update({disease_name:symptoms})
+    return dict
+
+def ask_for_symptoms(filename):
+    symptoms = []
+    with open(filename) as f:
+        for line in f:
+            answer = input("Are you experiencing %s? (Y/N)" % line)
+            valid_answer = False
+            while(valid_answer):
+                if answer.lower() == "y":
+                    valid_answer = True
+                    symptoms.append(answer)
+                elif answer.lower() == "n":
+                    valid_answer = True
+                else:
+                    answer = input("Invalid input. Please answer 'y' or 'n' to the above question.")
+    return symptoms
+
+def match_to_diseases(symptoms):
+    diseases = []
+    return diseases
+
+disease_dict = input_to_dict("input.txt")
+symptoms = ask_for_symptoms("symptoms.txt")
+possible_diagnoses = match_to_diseases(symptoms, disease_dict)
+print("You could be experiencing one of the following diseases:")
+for disease in possible_diagnoses:
+    print(disease)
